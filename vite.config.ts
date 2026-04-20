de5cc4c1829d3b71bf9bbb32c59067c2c085d883
ozzacking <ozzacking@gmail.com>
2026-04-20 01:57:00 -0700

    Remove base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jin/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react()],
+    optimizeDeps: {
+          exclude: ['lucide-react'],
+    },
 });
